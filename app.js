a5403fccc2872467100386a60d20efdf161a2fa5
Mustafa-ali123 <mustafalicoder@gmail.com>
2022-09-02 06:43:28 -0700

    code

---
+++ app.js
@@ -0,0 +1,341 @@
+// alert( "welcome the Mustafa");
+// alert("Welcome to JS land..\nHappy coding !");
+// alert("Wellcome to JS land ");
+// alert("hello,, i can run though my web browser console");
+// console.log ("hello,, i can run though my web browser console");
+
+
+ // chapter # 1 Comlete
+
+// var myname = "Mustafa";
+// alert(myname);
+// var Age = "15 year old";
+// alert(Age);
+// var cetificate = "Mobile App and Web Developer";
+// alert(cetificate);
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+//var email = "My email address is muhammadmustafa@gmail.com";
+//alert(email);
+// var book = " I am trying to learn the book A smarter way to learn JavaScript";
+// alert(book);
+//document.write("Tah! i can write HTML..content on the JavaScript");
+
+                    
+
+ // chapter # 2 complete
+
+
+// var a = +prompt("enter the value","Input Number");
+// var b = +prompt("enter the value","Input Number");
+// var c = (a+b);
+// alert(c);
+
+// var a = prompt("enter the value","Input Number");
+// var b = prompt("enter the value","Input Number");
+// var c = (a-b);
+// alert(c);
+
+// var a = prompt("enter the value","Input Number");
+// var b = prompt("enter the value","Input Number");
+// var c = (a*b);
+// alert(c);
+
+// var a = prompt("enter the value","Input Number");
+// var b = prompt("enter the value","Input Number");
+// var c = (a/b);
+// alert(c);
+
+// var a = prompt("cost of onwe movie ticket","Input Number");
+// var b = prompt("Buying ticket","Input Number");
+// var c = (a*b);
+// alert(c);
+
+// chap # 4 Comlete
+
+// var a = prompt("Obtained Mark","Input Number");
+// var b = prompt("Total Number","Input Number");
+// var c = (a/b)*100;
+// alert(c)
+
+// var a = prompt("Current Year","Input Number");
+// var b = prompt("Birth Year","Input Number");
+// var c = (a-b);
+// alert(c);
+
+// var a = prompt("Radius of circle","Input Number");
+// var b = 6.284;
+// var c = (a*b);
+// alert(c);
+
+//var a = prompt("Radius of circle","Input Number");
+// var b = 6.284;
+// var c = (a*b);
+// alert(c);
+
+
+// chapter # 5 complete
+
+// var a = 5;
+// var b = 10;
+// var c = (a - --b);
+// alert(c);
+
+// var a = 5;
+// var b = 10;
+// var c = (--a - --b);
+// alert(c);
+
+// var a = 5;
+// var b = 10;
+// var c = (--a - --b + ++b);
+// alert(c);
+
+// var a = 5;
+// var b = 10;
+// var c = (--a - --b + ++b + b--);
+// alert(c);
+ 
+// var a = prompt("Enter your name please!");
+// var b = " Sir Wellcome Please";
+// var c = (a+b);
+// alert(c);
+
+
+// var a = prompt("Table of 5","input number");
+// var b = 5;
+// var c = (a*b);
+// alert(c);
+
+
+// chapter (6-9) Comlete
+
+
+// var a = [
+//    "lahore","karachi"
+// ];
+//  var b = prompt("Enter the city name");
+
+// if("karachi");
+// alert("Wellcome to city of light"); 
+
+// if ("lahore")
+// alert ("wellcome the lahore")
+
+
+ 
+//  var a = [
+//     "Male","Female"
+// ];
+//   var b = prompt("Enter the Gender");
+
+// if("Male"){
+//  alert("Good Morning Sir")
+// }
+// else if("Female"){
+//  alert("Good Morning Maam")
+// }
+
+//  var a = ["red","green","yellow"];
+//  var b = prompt("Enter the road singnal color");
+
+//  if ("greem")
+//  {
+//     alert ("now move")
+//    };
+  
+//    if ("yellow"){
+//     alert ("ready to move")
+//    };
+    
+//   if ( "red"){
+//       alert ("must stop")
+//      };
+
+//  var a = ["0.25"];
+//  var b = prompt("Current fuel position");
+
+//  if ("0.25")
+//  {
+//     alert ("refill the fuel")
+//    };
+
+// var a = [
+//    "1",
+
+   
+//  ];
+//  var b = prompt("Gusess the secret number 1-10");
+//  var flag = false;
+ 
+//  for (var i = 0; i < a.length; i++) {
+//    if(a[i] == b){
+//        flag = true;
+//      break;
+//  }
+//  }
+ 
+//  if (flag) {
+//      alert("Bingo! Correct Answer");
+ 
+//  } else{
+//      alert("1 is the secret number it's correct answer");
+//  }
+
+
+// var number1 = prompt("Enter the first number");
+// var operator = prompt("enter the operater","+,-,*,/");
+// var number2 = prompt("Enter the second number");
+
+// if (operator == "+"){
+//    result = number1 + number2;
+// }
+
+//  else if (operator == "-"){
+//     result = number1 - number2;
+// }
+
+// else if(operator == "*"){
+//     result = number1 * number2;
+// }
+// else if(operator == "/"){
+//     result = number1 / number2;
+// }
+//  alert(`${number1} ${operator} ${number2} = ${result}`);
+
+// program to check if the number is even or odd
+// take input from the user
+
+
+// var number = prompt("Enter the number");
+// if (number % 2 == 0){
+//     alert("Number is even");
+// }
+// else {
+//     alert("Number is odd");
+// }
+
+
+// var temp = prompt("Enter the temprture");
+// if (temp > 40){
+//     alert ("It is too hot outside.");
+// }
+// else if (temp > 30){
+//     alert("The Weather today is Normal.");
+// }
+
+// else if (temp > 20){
+//     alert("Today Weather is cool.");
+// }
+// else if (temp > 10){
+//     alert("OMG! Today’s weather is so Cool.");
+// }
+
+
+// chap # 9-11 complete
+
+
+// var num = prompt("Enter the number");
+// if (num == "z" ){
+//     alert("147");
+// }
+// else if (num =="Z"){
+//     alert("258");
+// }
+// else if (num == "A"){
+//      alert("369");
+// }
+// else if (num == "a"){
+//     alert("159");
+// }
+
+// var num1, num2;
+// num1 = prompt("Input the First integer", "0");
+// num2 = prompt("Input the second integer", "0");
+                                                 
+// if(parseInt(num1, 10) > parseInt(num2, 10)) 
+//   { 
+//   alert("The larger of "+ num1+ " and "+ num2+ " is "+ num1+ ".");
+//   }   
+// else
+//   if(parseInt(num2, 10) > parseInt(num1, 10)) 
+//   {
+//  alert("The larger of "+ num1+" and "+ num2+ " is "+ num2+ ".");
+//   }                  
+// else
+//   {
+//    alert("The values "+ num1+ " and "+num2+ " are equal.");
+//   }
+
+// var num = prompt("Enter the number");
+// if (num == "-" ){
+//     alert("Your number is negative");
+// }
+// else if (num =="+"){
+//     alert("Your number is positive");
+// }
+// else if (num == "0"){
+//      alert("Your number is zero");
+// }
+
+// var vowe = prompt("Enter the letter");
+// if (vowe == "aeiou"){
+//     alert("True");
+// }
+// else{
+//     alert("False")
+// }
+// var a = 21245
+// var num = prompt("Enter the Password");
+// if (num == a ){
+//     alert("Password is correct!");
+// }
+// else {
+//     alert("Sorry sir password is Incorrect!");
+// }
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// }
+// else{
+// greeting = "Good evening";
+// }
+
+// var time = prompt("Enter the Time");
+// if (time < "6 am" ){
+//     alert("Good morning");
+// }
+// else if (time < "12 pm"){
+//     alert("Good Afternoon ");
+// }
+// else if (time < "8 pm"){
+//      alert("Good Evening");
+// }
+// else if (time < "10 pm"){
+//     alert("Good Night");
+// }
+
+// chap # (12-13)
+
+
+// var arr = [];
+// var arr = ["0","1","2","3","1","0","1","1","2","2","1","0","1"];
+// alert(arr);
+
+//  var a = "This is my cat.";
+//  alert(a);
+
+// var a = [
+//     "oranges",
+//     "atawberry",
+//     "banana",
+//     "mangoes",
+//     "apple",
+//   ];
+//   for (var i = 0; i < a.length; i++) {
+//   {
+//       console.log(a[i]);
+//     }
+// }
+
